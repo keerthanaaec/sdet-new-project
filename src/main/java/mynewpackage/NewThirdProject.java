fc24cce7db703e332c66d68f104f7cbd03abcc3c
keerthana <keerthanaaec@gmail.com>
2022-10-18 15:35:36 -0700

    commit

--- src/main/java/mynewpackage/NewThirdProject.java
+++ src/main/java/mynewpackage/NewThirdProject.java
@@ -1,7 +1,13 @@
 package mynewpackage;
 
-public class MyThirdProject {
+import java.awt.*;
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class NewThirdProject {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello");
+
     }
 }
